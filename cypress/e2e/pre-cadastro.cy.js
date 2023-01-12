9cc56c681ccc8750ff3c5f6e48b94b88bd65f7e5
fabio Bruno <messinasm88@gmail.com>
2023-01-12 00:19:23 -0300

    usando custom command

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,15 +1,17 @@
 /// <reference types="cypress" />
 
 import { faker } from '@faker-js/faker';
+import Faker from 'faker/lib';
+import Fake from 'faker/lib/fake';
 
 describe('Funcionalidade Pré Cadastro', () => {
 
     
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
     
-it('deve completar o pré cadastro com sucesso', () => {
+it.only('deve completar o pré cadastro com sucesso', () => {
 let nomeFaker = faker.name.firstName()
 let sobrenomeFaker = faker.name.lastName()
 let emailFaker = faker.internet.email(nomeFaker)
@@ -28,5 +30,11 @@ let emailFaker = faker.internet.email(nomeFaker)
 
 });
 
+it('Deve completar o pré-cadastro com sucesso usando Comandos customizados', () => {
+    let emailFaker2 = faker.internet.email()
+    cy.preCadastro('emailFaker2', 'senha!@forte', 'Fábio', 'Bruno' )
+
+    cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
+})
 
 });
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade Página de Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos/')
     });
 
     it('Deve selecionar um produto da lista', () => {
@@ -28,4 +28,14 @@ describe('Funcionalidade Página de Produtos', () => {
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
         cy.get('.woocommerce-message').should('contain', quantidade + " × “Abominable Hoodie” foram adicionados no seu carrinho.")
     });
+
+    it('Deve adicionar ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Ajax Full-Zip Sweatshirt', 'XS', 'Red', 1)
+    });
+
+    it('Deve adicionar ao carrinho - Usando Comando customizado', () => {
+        cy.addProdutos('Abominable Hoodie', 'XL', 'Green', 4)
+    });
+
+
 });
