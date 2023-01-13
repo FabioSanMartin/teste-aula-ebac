a7c3c42400bf435b480545d888649674e2a918be
fabio Bruno <messinasm88@gmail.com>
2023-01-12 23:57:36 -0300

    finalizada a automacao

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'cpcr33',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/"
   },
--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,19 +1,53 @@
 /// <reference types="cypress" />
-import enderecoPage from "../support/page-objects/endereco.page";
+import enderecoPage from '../support/page-objects/endereco.page';
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 beforeEach(() => {
     cy.visit('minha-conta/')
     cy.fixture('perfil').then((dados) => {
         cy.login(dados.usuario, dados.senha)
-    
-        
+            
     });
     
 });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         enderecoPage.editarEnderecoFaturamento('Fábio', 'Bruno', 'Google', 'Brasil', 'Av João', '238', 'Paranagua', 'São Paulo', '83209212', '419999955', 'teste@teste.com')
         cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer o cadastro de entrega com sucesso', () => {
+        enderecoPage.editarEnderecoEntrega(
+    
+        dadosEndereco[2].nome,
+        dadosEndereco[2].sobrenome,
+        dadosEndereco[2].empresa,
+        dadosEndereco[2].pais,
+        dadosEndereco[2].endereco,
+        dadosEndereco[2].numero,
+        dadosEndereco[2].cidade,
+        dadosEndereco[2].estado,
+        dadosEndereco[2].cep,
+        
+        )
+    });
+
 });
